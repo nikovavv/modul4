f82b16ba68b9612bc2bbf8e512653d8e596a2f03
nikovavv <143206373+nikovavv@users.noreply.github.com>
2025-04-18 01:38:04 +0300

    Add files via upload
    сайт про телескоп

---
+++ script.php
@@ -0,0 +1,13 @@
+<?php
+require 'head.htm';
+?>
+<div class="img" style=" top:275; left:200; width:100%;  min-width: 100%;  max-width: 100%; position: absolute; ">
+
+<p> <img src="tel.jpg" alt="Paris"></font>
+</p>
+</div>
+
+
+<?php
+require 'foot.htm';
+?>
